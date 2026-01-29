06c71dbbc37811daad5b67e59289277fae357198
Hindavidevkate <hindavidevkate3@gmail.com>
2026-01-29 19:48:11 +0530

    Formula are correctly added

--- recursion.cpp
+++ recursion.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-int binartSearch(vector<int>arr,int tar,int st,int end){
-    int mid = st + (st + end)/2;
+int binarySearch(vector<int>arr,int tar,int st,int end){
+    int mid = st + (end - st)/2;
     if(st <= end){
         if(arr[mid] == tar){
             return mid;
         }
         if(arr[mid] < tar){
-            return binartSearch(arr, tar,mid + 1,end);
+            return binarySearch(arr, tar,mid + 1,end);
 
         }
         if(arr[mid] > tar){
-            return binartSearch(arr, tar,st,mid+1);
+            return binarySearch(arr, tar,st,mid+1);
         }
     }
     return -1;
@@ -22,6 +22,6 @@ int binartSearch(vector<int>arr,int tar,int st,int end){
 int main(){
     vector<int> arr = {1, 3 ,5,6,7};
     int tar = 5;
-    cout<<binartSearch(arr, tar,0,arr.size());
+    cout<<binarySearch(arr, tar,0,arr.size());
     return 0;
 }
